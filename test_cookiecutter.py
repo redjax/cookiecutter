7fb080be31ca905020cf2421a0febbf196c7c2cb
redjax <none@none.com>
2024-10-31 11:51:10 -0400

    Update PDM template, fix |replace() line.
    Add sandbox/ dir to .gitignore.
    
    Add test_cookiecutter.py, a script I will add to pipelines to test rendering each template to ensure there are no templating errors

--- test_cookiecutter.py
+++ test_cookiecutter.py
@@ -4,23 +4,66 @@ from pathlib import Path
 
 from cookiecutter.main import cookiecutter
 
+log = logging.getLogger(__name__)
 
-def render_uv(output_dir: str = "test", no_input: bool = False):
-    output_dir: Path = (
-        Path(str(output_dir)).expanduser()
-        if "~" in str(output_dir)
-        else Path(str(output_dir))
+SANDBOX_DIR = Path("sandbox")
+
+
+def prepare_sandbox(sandbox_dir: str = SANDBOX_DIR):
+    if not Path(str(sandbox_dir)).exists():
+        Path(str(sandbox_dir)).mkdir(parents=True, exist_ok=True)
+
+
+def render_cookicutter(
+    template: str,
+    output_dir: str = ".",
+    template_dir: str | None = None,
+    extra_context: dict | None = None,
+    checkout_branch: str | None = None,
+    no_input: bool = False,
+    overwrite: bool = False,
+):
+
+    log.info(
+        f"Rendering cookiecutter template '{template}' to output directory '{output_dir}'"
     )
+    try:
+        cookiecutter(
+            template=template,
+            checkout=checkout_branch,
+            no_input=no_input,
+            extra_context=extra_context,
+            overwrite_if_exists=False,
+            output_dir=output_dir,
+            directory=template_dir,
+        )
+    except Exception as exc:
+        msg = f"({type(exc)}) Error rendering cookiecutter template. Details: {exc}"
+        log.error(msg)
 
-    if not Path(str(output_dir)).exists():
-        output_dir.mkdir(parents=True, exist_ok=True)
-
-    cookiecutter(template="t/python/uv", output_dir=output_dir, no_input=no_input)
+        raise exc
 
 
 def main(no_input: bool = False):
-    render_uv(no_input=no_input)
+    UV_TEMPLATE_PATH = "t/python/uv"
+    PDM_TEMPLATE_PATH = "t/python/pdm"
+
+    ## Test rendering Python UV cookiecutter
+    # render_cookicutter(
+    #     template=UV_TEMPLATE_PATH, no_input=no_input, output_dir=SANDBOX_DIR
+    # )
+
+    ## Test rendering Python PDM cookiecutter
+    render_cookicutter(
+        template=PDM_TEMPLATE_PATH, no_input=no_input, output_dir=SANDBOX_DIR
+    )
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level="INFO",
+        format="%(asctime)s | [%(levelname)s] > %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S",
+    )
+
     main(no_input=True)
